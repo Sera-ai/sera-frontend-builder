2c47942a6c1b52ca3f5814f2b65e4bfb6b38194b
rakeda <r.rakeda@gmail.com>
2023-12-20 00:27:48 +0000

    vite changes

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,2 +1,2 @@
 import { io } from 'socket.io-client';
-export const socket = io(`http://localhost:${process.env.BE_ROUTER_PORT}`);
+export const socket = io(`http://localhost:${__BE_ROUTER_PORT__}`);
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
-  },
-  define: {
+    '__BE_ROUTER_PORT__': JSON.stringify(process.env.BE_ROUTER_PORT),
     "global": {}
   },
   plugins: [react()],
