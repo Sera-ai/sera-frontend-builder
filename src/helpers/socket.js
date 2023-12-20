8060f586c46d7b3f242f87881396bdb8101ee181
rakeda <r.rakeda@gmail.com>
2023-12-20 00:46:11 +0000

    vite changes

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -1,2 +1,2 @@
 import { io } from 'socket.io-client';
-export const socket = io(`http://localhost:${__BE_ROUTER_PORT__}`);
+export const socket = io(`http://${window.location.hostname}:${__BE_ROUTER_PORT__}`);
